4e21b5d6313ef547f8920b8e3a3798a37ff4385a
Nhat Minh <minhndn.191@gmail.com>
2026-04-28 11:04:03 +0700

    di example

---
+++ Tổng quan về Spring & Khái niệm/di-implementation/src/Main.java
@@ -0,0 +1,28 @@
+
+public static class A {
+    private final IDependency dependency;
+    A(IDependency dependency) {
+        this.dependency = dependency;
+    }
+
+    public void doSmt() {
+        dependency.check();
+    }
+}
+
+public interface IDependency {
+    void check();
+}
+
+public static class DependencyImpl implements IDependency {
+    @Override
+    public void check() {
+        IO.println("DependencyImpl");
+    }
+}
+
+void main() {
+    IDependency dep = new DependencyImpl();
+    A a = new A(dep);
+    a.doSmt();
+}
