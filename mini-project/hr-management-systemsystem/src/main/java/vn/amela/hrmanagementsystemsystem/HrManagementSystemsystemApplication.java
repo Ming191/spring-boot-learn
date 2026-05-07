52773ac5f17c5cb4b44f4f22303469d4498f56cd
Nhat Minh <minhndn.191@gmail.com>
2026-05-07 16:16:09 +0700

    initialize Spring Boot project for HR Management System with Gradle setup, basic structure, and Eureka client integration

---
+++ mini-project/hr-management-systemsystem/src/main/java/vn/amela/hrmanagementsystemsystem/HrManagementSystemsystemApplication.java
@@ -0,0 +1,13 @@
+package vn.amela.hrmanagementsystemsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementSystemsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrManagementSystemsystemApplication.class, args);
+    }
+
+}
