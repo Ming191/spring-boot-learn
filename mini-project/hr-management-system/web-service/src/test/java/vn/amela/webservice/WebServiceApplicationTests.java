2304366b08f03f51d1131ec2642dbf17366d3f8b
Nhat Minh <minhndn.191@gmail.com>
2026-05-20 10:31:47 +0700

    initialize web service project; add application configuration and Gradle setup

---
+++ mini-project/hr-management-system/web-service/src/test/java/vn/amela/webservice/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.amela.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = "eureka.client.enabled=false")
+class WebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
